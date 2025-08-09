7c261d8eda021aa16f42ec028ec0a66ce6ed8b4b
massan-E <masahiro.greed@gmail.com>
2025-08-10 01:24:41 +0900

    link.sh修正

--- .zprofile
+++
@@ -1,3 +0,0 @@
-
-# Created by `pipx` on 2025-06-10 00:52:33
-export PATH="$PATH:/home/massanc/.local/bin"
--- link.sh
+++ link.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
 ln -s ~/dotfiles/.zshrc ~/
-ln -s ~/dotfiles/.zprofile ~/
-ln -s ~/dotfiles/.bashrc ~/
 ln -s ~/dotfiles/.config/nvim ~/.config
